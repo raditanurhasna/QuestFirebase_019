6d9b1e8ba77758bbc90bea15c28212241f78eabc
raditanurhasna <raditanurhasna1410@gmail.com>
2025-01-10 18:59:56 +0700

    Menambahkan fungsi untuk mengupdate data mahasiswa.

--- app/src/main/java/com/example/pertemuan13/repository/NetworkRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan13/repository/NetworkRepositoryMhs.kt
@@ -63,8 +63,17 @@ class NetworkRepositoryMhs(
     }
 
     override suspend fun updateMhs(mahasiswa: Mahasiswa) {
-        TODO("Not yet implemented")
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .set(mahasiswa)
+                .await()
+
+        }  catch (e: Exception) {
+            throw Exception("Gagal mengupdate data mahasiswa: ${e.message}")
+        }
     }
 
-
 }
+
+
