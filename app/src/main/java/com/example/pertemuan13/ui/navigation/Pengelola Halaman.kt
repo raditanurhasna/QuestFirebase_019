3da8f27e97c1ae00165ca3b709d14dfcc35c508f
raditanurhasna <raditanurhasna1410@gmail.com>
2025-01-11 00:10:31 +0700

    Menambahkan Pengelola Halaman

---
+++ app/src/main/java/com/example/pertemuan13/ui/navigation/Pengelola Halaman.kt
@@ -0,0 +1,39 @@
+package com.example.pertemuan13.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan13.ui.home.pages.HomeScreen
+import com.example.pertemuan13.ui.home.pages.InsertMhsView
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToltemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
